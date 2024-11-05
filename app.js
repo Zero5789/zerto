0c5f9a2dd104fc6c11f749c14418bd64380ae958
Zero5789 <onayamisoudan48@gmail.com>
2024-11-05 22:42:12 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const answers =[
     "ニンテンドーDS"
 ];
 const correct ="ニンテンドーDS";
-
+const $button =document.getElementsByTagName("button");
 const setupQuiz = () => {
     document.getElementById("js-question").textContent =question;
     let buttonIndex =0;
@@ -19,36 +19,20 @@ const setupQuiz = () => {
 
 setupQuiz ();
 
-//ボタンをクリックしたら正誤判定
-document.getElementsByTagName("button")[0].addEventListener("click", () => {
-    if(correct === document.getElementsByTagName("button")[0].textContent) {
+const clicklerIndex = (e) => {
+    if(correct ===e.target.textContent) {
         window.alert("正解");
     }
     else {
         window.alert("不正解");
     }
-})
-document.getElementsByTagName("button")[1].addEventListener("click", () => {
-    if(correct === document.getElementsByTagName("button")[1].textContent) {
-        window.alert("正解");
-    }
-    else {
-        window.alert("不正解");
-    }
-})
-document.getElementsByTagName("button")[2].addEventListener("click", () => {
-    if(correct === document.getElementsByTagName("button")[2].textContent) {
-        window.alert("正解");
-    }
-    else {
-        window.alert("不正解");
-    }
-})
-document.getElementsByTagName("button")[3].addEventListener("click", () => {
-    if(correct === document.getElementsByTagName("button")[3].textContent) {
-        window.alert("正解");
-    }
-    else {
-        window.alert("不正解");
-    }
-})
+};
+
+let handlerIndex = 0;
+const buttonLength = $button.length;
+while (handlerIndex < $button.length) {
+    $button[handlerIndex].addEventListener("click" , (e) => {
+        clicklerIndex(e);
+    })
+    handlerIndex++;
+}
