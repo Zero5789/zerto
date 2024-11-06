06e16435e34f38c5fce06a8dde1b1b2a1d9fde2d
Zero5789 <onayamisoudan48@gmail.com>
2024-11-06 11:30:52 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,48 @@
-const question ="ゲーム市場、最も売れたゲーム機はどれですか";
-const answers =[
-    "スーパーファミコン", 
-    "プレイステーション2", 
-    "ニンテンドースイッチ", 
-    "ニンテンドーDS"
+
+const quiz = [
+    {
+         question :"ゲーム市場、最も売れたゲーム機はどれですか",
+         answers :[
+            "スーパーファミコン", 
+            "プレイステーション2", 
+            "ニンテンドースイッチ", 
+            "ニンテンドーDS"
+         ], 
+         correct:"ニンテンドーDS"
+    },
+    {
+        question :"糸井重里が企画に関わった、任天堂の看板ゲームといえば？",
+        answers :[
+           "MOTHER2", 
+           "スーパーマリオブラザーズ3", 
+           "スーパードンキーコング", 
+           "星のカービー"
+        ], 
+        correct:"MOTHER2"
+   },
+   {
+        question :"ファイナルファンタジーⅣの主人公の名前は？",
+        answers :[
+           "フリオニール", 
+           "クラウド", 
+           "ティーダ", 
+           "セシル"
+        ], 
+        correct:"セシル"
+},
 ];
-const correct ="ニンテンドーDS";
+
+const quizLength =quiz.length;
+let quizIndex = 0;
+let score = 0;
+
 const $button =document.getElementsByTagName("button");
 const setupQuiz = () => {
-    document.getElementById("js-question").textContent =question;
+    document.getElementById("js-question").textContent =quiz[quizIndex].question;
     let buttonIndex =0;
     let buttonLength = document.getElementsByTagName("button").length;
     while(buttonIndex < buttonLength) {
-       document.getElementsByTagName("button")[buttonIndex].textContent = answers[buttonIndex];
+       document.getElementsByTagName("button")[buttonIndex].textContent = quiz[quizIndex].answers[buttonIndex];
        buttonIndex++;
     }
 }
@@ -20,12 +50,23 @@ const setupQuiz = () => {
 setupQuiz ();
 
 const clicklerIndex = (e) => {
-    if(correct ===e.target.textContent) {
+    if(quiz[quizIndex].correct ===e.target.textContent) {
         window.alert("正解");
+        score++;
     }
     else {
         window.alert("不正解");
     }
+
+    quizIndex++;
+
+    if(quizIndex < quizLength) {
+        setupQuiz();
+    }
+    else{
+        window.alert("終了 あなたの正解数は" + score + "/" + quizLength + "です。");
+    }
+
 };
 
 let handlerIndex = 0;
